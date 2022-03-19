bac155fafb40e8e70a3f114dc6d762e46ef15881
Mariana Cruz <65827628+marianafcruz17@users.noreply.github.com>
2022-03-18 21:33:25 -0300

    Add files via upload

---
+++ exercicio1.c
@@ -0,0 +1,33 @@
+// Diga qual será a quantidade de operações realizadas para as seguintes instâncias do problema:
+
+// a) vet[1,2,3,4,5,6,7,8,9,10] -> quantidade de instruções: 55
+// b) vet[10,20,30,40,50,60,70] -> quantidade de instruções: 31
+// c) vet[1,3,5,7,9,11,13,15,17] -> quantidade de instruções: 59
+//a[] -> 10
+//b[] -> 7
+//c[] -> 9
+
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM1 10
+#define TAM2 10
+
+int main()
+{
+    int i,j,x; //0
+    int vet[]=x; //1
+    for(i=0;i<TAM1;i++) //1 + (10 + 1) + 10 = 22 | 1 + (7 + 1) + 7 = 16 | 1 + (9 + 1) + 9 = 20
+    {
+        if (vet[i]%3==0) //1 x 10 = 10 | 1 x 7 = 7 | 1 x 9 = 9
+        {
+            for(j=0;j<TAM2;j++) //1 + (5 + 1) + 5 = 12 | 0 | 1 + (9 + 1) + 9 = 20
+            {
+                printf("\n Senhor"); //1 x 5 = 5 | 0 | 1 x 9 = 9
+            }
+        }
+        else
+        {
+            printf("\n Gzuz"); //1 x 5 = 5 | 1 x 7 = 7 | 0
+        }
+    }
+}
