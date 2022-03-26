b424b1507d99d8eca686a4e92c2f585777a87119
Mariana Cruz <65827628+marianafcruz17@users.noreply.github.com>
2022-03-25 21:23:43 -0300

    Add files via upload

---
+++ aula 3.2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int i;
+    int n = 100;
+    
+    for(i=100;i>=1;i=1/2){
+        printf("\n teste");
+    }
+}
+
+//complexidade do código: log(n)
